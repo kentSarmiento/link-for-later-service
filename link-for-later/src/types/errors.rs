e207cd60673c6b2a7ca38dadfb6b533e40374d14
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2023-12-21 15:00:41 +0800

    feat: user login (#20)

--- link-for-later/src/types/errors.rs
+++ link-for-later/src/types/errors.rs
@@ -3,11 +3,12 @@ use std::{error, fmt};
 #[derive(Debug)]
 pub enum App {
     NotSupported,
-
+    ServerError,
     DatabaseError,
     ItemNotFound,
     UserAlreadyExists,
     UserNotFound,
+    InvalidPassword,
 
     #[cfg(test)]
     TestError,
@@ -17,10 +18,12 @@ impl fmt::Display for App {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::NotSupported => write!(f, "operation not supported"),
+            Self::ServerError => write!(f, "server error"),
             Self::DatabaseError => write!(f, "database error"),
             Self::ItemNotFound => write!(f, "item not found"),
             Self::UserAlreadyExists => write!(f, "user already exist"),
             Self::UserNotFound => write!(f, "user not found"),
+            Self::InvalidPassword => write!(f, "incorrect password for user"),
             #[cfg(test)]
             Self::TestError => write!(f, "test error"),
         }
--- link-for-later/src/types/response.rs
+++ link-for-later/src/types/response.rs
@@ -3,18 +3,24 @@ use axum::{
     response::{IntoResponse, Response},
     Json,
 };
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
-use super::AppError;
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Login {
+    pub token: String,
+}
 
-impl IntoResponse for AppError {
+impl IntoResponse for super::AppError {
     fn into_response(self) -> Response {
         let (status, error_message) = match self {
             Self::NotSupported => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
+            Self::ServerError => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
             Self::DatabaseError => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
             Self::ItemNotFound => (StatusCode::NOT_FOUND, self.to_string()),
             Self::UserAlreadyExists => (StatusCode::BAD_REQUEST, self.to_string()),
             Self::UserNotFound => (StatusCode::BAD_REQUEST, self.to_string()),
+            Self::InvalidPassword => (StatusCode::UNAUTHORIZED, self.to_string()),
             #[cfg(test)]
             Self::TestError => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
         };
