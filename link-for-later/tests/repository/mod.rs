7bbf19c9859bbef73205e9a2e10cbe4b07098227
Kent Tristan Yves Sarmiento <kenttristan.sarmiento@iress.com>
2023-12-25 14:03:22 +0000

    refactor repository utils in tests

--- link-for-later/tests/repository/mod.rs
+++ link-for-later/tests/repository/mod.rs
@@ -1,106 +1,68 @@
 #![allow(dead_code)]
-use std::str::FromStr;
-
-use bson::{doc, oid::ObjectId, Bson};
-use mongodb::{options::ClientOptions, Client, Database};
-use rand::Rng;
 
 use crate::entity::{LinkItem, UserInfo};
 
-const MONGODB_URI_KEY: &str = "MONGODB_URI";
-const MONGODB_DATABASE_NAME_KEY: &str = "MONGODB_DATABASE_NAME";
-const LINKS_COLLECTION_NAME_KEY: &str = "LINKS_COLLECTION_NAME";
-const USERS_COLLECTION_NAME_KEY: &str = "USERS_COLLECTION_NAME";
+pub mod mongodb;
 
-pub fn setup() {
-    let mut rng = rand::thread_rng();
-    let id = rng.gen::<u32>();
-
-    let links_collection = format!("{}/links", id);
-    std::env::set_var(LINKS_COLLECTION_NAME_KEY, links_collection);
-
-    let users_collection = format!("{}/users", id);
-    std::env::set_var(USERS_COLLECTION_NAME_KEY, users_collection);
+#[derive(Clone)]
+pub enum DatabaseType {
+    MongoDb,
+    InMemory,
 }
 
-pub async fn database() -> Database {
-    let uri = std::env::var(MONGODB_URI_KEY).unwrap();
-    let database_name = std::env::var(MONGODB_DATABASE_NAME_KEY).unwrap();
-
-    let client_options = ClientOptions::parse(uri).await.unwrap();
-    let client = Client::with_options(client_options).unwrap();
-    client.database(&database_name)
+pub struct Repository {
+    db_type: DatabaseType,
 }
 
-pub async fn count_links() -> u64 {
-    let collection = std::env::var(LINKS_COLLECTION_NAME_KEY).unwrap();
-    let collection = database().await.collection::<LinkItem>(&collection);
-
-    collection.count_documents(None, None).await.unwrap()
-}
-
-pub async fn get_link(id: &str) -> LinkItem {
-    let collection = std::env::var(LINKS_COLLECTION_NAME_KEY).unwrap();
-    let collection = database().await.collection(&collection);
-
-    let id = if let Ok(id) = bson::oid::ObjectId::from_str(id) {
-        id
-    } else {
-        ObjectId::default()
+pub fn new(db_type: &DatabaseType) -> Repository {
+    let db_type = db_type.clone();
+    match db_type {
+        DatabaseType::InMemory => {}
+        DatabaseType::MongoDb => mongodb::setup(),
     };
-    let db_query = doc! {"_id": id};
-    collection.find_one(db_query, None).await.unwrap().unwrap()
+    Repository { db_type }
 }
 
-pub async fn add_link(owner: &str, url: &str) -> String {
-    let collection = std::env::var(LINKS_COLLECTION_NAME_KEY).unwrap();
-    let collection = database().await.collection(&collection);
+impl Repository {
+    pub async fn count_links(&self) -> u64 {
+        match self.db_type {
+            DatabaseType::InMemory => unimplemented!(),
+            DatabaseType::MongoDb => mongodb::count_links().await,
+        }
+    }
 
-    let document = doc! {"id": "1", "owner": owner, "url": url, "title": "", "description": "", "created_at": "", "updated_at": ""};
-    let result = collection.insert_one(document, None).await.unwrap();
+    pub async fn get_link(&self, id: &str) -> LinkItem {
+        match self.db_type {
+            DatabaseType::InMemory => unimplemented!(),
+            DatabaseType::MongoDb => mongodb::get_link(id).await,
+        }
+    }
 
-    let id = if let Bson::ObjectId(id) = result.inserted_id {
-        id.to_hex()
-    } else {
-        String::default()
-    };
-    let query = doc! {"_id": result.inserted_id.clone()};
-    let update = doc! {"$set": doc! { "id": &id } };
-    collection.update_one(query, update, None).await.unwrap();
+    pub async fn add_link(&self, owner: &str, url: &str) -> String {
+        match self.db_type {
+            DatabaseType::InMemory => unimplemented!(),
+            DatabaseType::MongoDb => mongodb::add_link(owner, url).await,
+        }
+    }
 
-    id
-}
+    pub async fn count_users(&self) -> u64 {
+        match self.db_type {
+            DatabaseType::InMemory => unimplemented!(),
+            DatabaseType::MongoDb => mongodb::count_users().await,
+        }
+    }
 
-pub async fn count_users() -> u64 {
-    let collection = std::env::var(USERS_COLLECTION_NAME_KEY).unwrap();
-    let collection = database().await.collection::<UserInfo>(&collection);
+    pub async fn get_user(&self, email: &str) -> UserInfo {
+        match self.db_type {
+            DatabaseType::InMemory => unimplemented!(),
+            DatabaseType::MongoDb => mongodb::get_user(email).await,
+        }
+    }
 
-    collection.count_documents(None, None).await.unwrap()
-}
-
-pub async fn get_user(email: &str) -> UserInfo {
-    let collection = std::env::var(USERS_COLLECTION_NAME_KEY).unwrap();
-    let collection = database().await.collection(&collection);
-
-    let db_query = doc! {"email": email};
-    collection.find_one(db_query, None).await.unwrap().unwrap()
-}
-
-pub async fn add_user(email: &str, password: &str) -> String {
-    let collection = std::env::var(USERS_COLLECTION_NAME_KEY).unwrap();
-    let collection = database().await.collection(&collection);
-
-    let document = doc! {"id": "1", "email": email, "password": password, "verified": true, "created_at": "", "updated_at": ""};
-    let result = collection.insert_one(document, None).await.unwrap();
-
-    let id = if let Bson::ObjectId(id) = result.inserted_id {
-        id.to_hex()
-    } else {
-        String::default()
-    };
-    let query = doc! {"_id": result.inserted_id.clone()};
-    let update = doc! {"$set": doc! { "id": &id } };
-    collection.update_one(query, update, None).await.unwrap();
-
-    id
+    pub async fn add_user(&self, email: &str, password: &str) -> String {
+        match self.db_type {
+            DatabaseType::InMemory => unimplemented!(),
+            DatabaseType::MongoDb => mongodb::add_user(email, password).await,
+        }
+    }
 }
